ae9118595f6d31f7f3e1b657e158f96c0de1b988
Manish Saini <manish@pop-os.localdomain>
2021-10-23 00:55:59 +0530

    api_view Decorator

--- restapi/app/views.py
+++ restapi/app/views.py
@@ -1,59 +1,59 @@
-from django.shortcuts import render, HttpResponse
-from django.http import JsonResponse
+#from django.shortcuts import render, HttpResponse
+#from django.http import JsonResponse
 # Create your views here.
 from .models import EmployeDetails
 from .serializers import EmployeSerializer, UserSerializer
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+#from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def index(request):
     if request.method == 'GET':
         employobj = EmployeDetails.objects.all()
         serializer = EmployeSerializer(employobj, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        jsonData = JSONParser().parse(request)
-        print(jsonData)
-        serializer = EmployeSerializer(data = jsonData)
+        serializer = EmployeSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors)
         #return JsonResponse({"Message":"Add Employee"}, safe=False)
 
+@api_view(['GET'])
 def UserListView(request):
     Userobj = User.objects.all()
     serializers = UserSerializer(Userobj, many=True)
-    return JsonResponse(serializers.data, safe=False)
+    return Response(serializers.data)
 
-@csrf_exempt
+@api_view(['GET', 'DELETE', 'PUT'])
 def EmployeeDetailView(request, pk):
     try:
         employee = EmployeDetails.objects.get(pk=pk)
     except EmployeDetails.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=404)
 
     if request.method == 'DELETE':
         employee.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     elif request.method == 'GET':
         serializer = EmployeSerializer(employee)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        jsonData = JSONParser().parse(request)
-        serializer = EmployeSerializer(employee, data = jsonData)
+        serializer = EmployeSerializer(employee, data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors)
 
 
 
